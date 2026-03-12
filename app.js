140797c1bf08b9990cbc51e4718cd9c9b77a14f9
Aku <86946460+MZmiao@users.noreply.github.com>
2026-03-12 13:35:25 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// app.js
+App({
+  onLaunch: function () {
+    // 小程序启动时执行的逻辑
+    console.log('小程序启动了');
+  },
+  globalData: {
+    // 全局数据
+    totalProfit: 0,
+    transactions: []
+  }
+})
---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+  fee: 0.004, // 默认手续费率 0.4%
+  banks: ['民生银行', '浙商银行']
+};
+
+module.exports = config; 
